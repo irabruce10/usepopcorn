c01b727c36ba7cb22a33711ba41db6a6d831e4fc
Bruce <irabruce10@gmail.com>
2024-02-19 10:24:57 +0100

    add item

--- src/Add.js
+++ src/Add.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 
+const data = [{ text: "Learn html" }, { text: "javascript" }];
+
 export default function Add() {
-  const [item, setItem] = useState([]);
+  const [item, setItem] = useState(data);
   const [newItem, setNewItem] = useState("");
 
   function handleAdd() {
@@ -24,17 +26,17 @@ function ItemList({ item, handleAdd }) {
       </div>
       <ul>
         {item.map((itm) => (
-          <Item key={itm.text} />
+          <Item key={itm.text} item={itm} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item() {
+function Item({ item }) {
   return (
     <li>
-      <p>text</p>
+      <p>{item.text}</p>
     </li>
   );
 }
