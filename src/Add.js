0285403d1290c0359115f96141f59fde302febed
Bruce <irabruce10@gmail.com>
2024-02-19 10:08:27 +0100

    add item

--- src/Add.js
+++ src/Add.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Add() {
+  const [item, setItem] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  function handleAdd() {
+    console.log("ke");
+  }
+
   return (
     <div>
-      <input />
-      <button>Add</button>
+      <ItemList item={item} handleAdd={handleAdd} />
     </div>
+  );
+}
+
+function ItemList({ item, handleAdd }) {
+  return (
+    <div>
+      <div>
+        <input />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+      <ul>
+        {item.map((itm) => (
+          <Item key={itm.text} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item() {
+  return (
+    <li>
+      <p>text</p>
+    </li>
   );
 }
