1a0c58be961a7c45566ae9f1c3e272b467544780
Bruce <irabruce10@gmail.com>
2024-02-19 09:56:15 +0100

    star rating

---
+++ src/Add.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Add() {
+  return (
+    <div>
+      <input />
+      <button>Add</button>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 import StartRating from "./StartRating";
+import Add from "./Add";
 // import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StartRating maxRating={5} />
+    {/* <StartRating maxRating={5} />
     <StartRating maxRating={10} />
-    <StartRating />
+    <StartRating /> */}
+
+    <Add />
   </React.StrictMode>
 );
