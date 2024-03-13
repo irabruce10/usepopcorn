50e0f576b2b77f7d18e6a8d5f0c6cc67d61068c9
Bruce <irabruce10@gmail.com>
2024-03-13 09:21:25 +0100

    Loading movie Details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./index.css";
+import StarRating from "./StartRating";
 
 const tempMovieData = [
   {
@@ -272,7 +273,22 @@ function FilmWatchedList({ moviesWatched, onSelect }) {
 }
 
 function MovieDetails({ selectId, onCloseMovie }) {
-  const 
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(null);
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
   useEffect(
     function () {
       async function getMovieDetails() {
@@ -281,7 +297,7 @@ function MovieDetails({ selectId, onCloseMovie }) {
         );
 
         const data = await res.json();
-        console.log(data);
+        setMovie(data);
       }
 
       getMovieDetails();
@@ -290,10 +306,41 @@ function MovieDetails({ selectId, onCloseMovie }) {
   );
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
-      {selectId}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Poster of ${movie} movie`} />
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐️</span>
+                {imdbRating} IMDb rating
+              </p>
+            </div>
+          </header>
+          <section>
+            <div className="rating">
+              <>
+                <StarRating maxRating={10} size={24} />
+              </>
+            </div>
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring {actors}</p>
+            <p>Directed by {director}</p>
+          </section>
+        </>
+      )}
     </div>
   );
 }
