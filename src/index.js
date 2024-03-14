891695ac9f67b9ff4be24d604033e2d43b82d890
Bruce <irabruce10@gmail.com>
2024-03-14 17:50:16 +0100

    adding watched movie

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-import StartRating from "./StartRating";
-import Add from "./Add";
-import App from "./App";
+import "./index.css";
+import V1 from "./V1";
+// import StartRating from "./StartRating";
+// import Add from "./Add";
+// import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <V1 />
+    {/* <App /> */}
     {/* <StartRating maxRating={5} />
     <StartRating maxRating={10} />
     <StartRating /> */}
